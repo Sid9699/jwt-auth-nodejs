597672a385d7548bb69f7c5b62e3a958f85aa607
siddharth <ranasiddharth6@gmail.com>
2022-11-30 20:30:59 +0530

    cors config

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 const port = 4000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", require("./routes/auth"));
